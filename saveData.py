0fa237228947d63a20d3a0c5d40d8af28ae8e5b8
pradheerkk <pradheerkk@gmail.com>
2019-12-16 13:19:37 +0530

    Create saveData.py
    the access credentials should be given in line no:5

---
+++ saveData.py
@@ -0,0 +1,50 @@
+import boto3
+import os
+import datetime
+
+s3_client = boto3.client('s3')
+
+bucket = 'bucket_Name'
+prefix = r'Folder_destination where files are located'
+#ex:data/data_load/example/example
+local = 'Local Folder Destination where Files can be Stores'
+#ex: 'D:\Data_Files'
+
+def download_dir(prefix, local, bucket, client):
+    """
+    params:
+    - prefix: pattern to match in s3
+    - local: local path to folder in which to place files
+    - bucket: s3 bucket with target contents
+    - client: initialized s3 client object
+    """
+    keys = []
+    dirs = []
+    next_token = ''
+    base_kwargs = {
+        'Bucket':bucket,
+        'Prefix':prefix,
+    }
+    while next_token is not None:
+        kwargs = base_kwargs.copy()
+        if next_token != '':
+            kwargs.update({'ContinuationToken': next_token})
+        results = client.list_objects_v2(**kwargs)
+        contents = results.get('Contents')
+        for i in contents:
+            k = i.get('Key')
+            if k[-1] != '/':
+                keys.append(k)
+            else:
+                dirs.append(k)
+        next_token = results.get('NextContinuationToken')
+    for d in dirs:
+        dest_pathname = os.path.join(local, d)
+        if not os.path.exists(os.path.dirname(dest_pathname)):
+            os.makedirs(os.path.dirname(dest_pathname))
+    for k in keys:
+        dest_pathname = os.path.join(local, k)
+        if not os.path.exists(os.path.dirname(dest_pathname)):
+            os.makedirs(os.path.dirname(dest_pathname))
+        client.download_file(bucket, k, dest_pathname)
+download_dir(prefix,local,bucket,s3_client)
